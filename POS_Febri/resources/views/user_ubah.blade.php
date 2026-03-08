77839bd91b5d2a8236fd95722cd92eb23b1e534c
m1Febriansyah <muhammadfebriansyah1000@gmail.com>
2026-03-09 01:14:19 +0700

    Praktikum2.6_P4

---
+++ POS_Febri/resources/views/user_ubah.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ubah Data User</title>
+</head>
+<body>
+    <h1>Form Ubah Data User</h1>
+    <a href="{{ url('/user') }}">Kembali</a>
+    <br><br>
+    <form method="post" action="{{ url('/user/ubah_simpan/' . $data->user_id) }}">
+        {{ csrf_field() }}
+        {{ method_field('PUT') }}
+
+        <label>Username</label>
+        <input type="text" name="username" value="{{ $data->username }}">
+        <br>
+        <label>Nama</label>
+        <input type="text" name="nama" value="{{ $data->nama }}">
+        <br>
+        <label>Level ID</label>
+        <input type="number" name="level_id" value="{{ $data->level_id }}">
+        <br><br>
+        <input type="submit" name="btnSimpan" value="Ubah">
+    </form>
+</body>
+</html>
